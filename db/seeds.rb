2f75d95a83d5efbaa7902481f7a5666f484b84c2
moffire <alexeydudarev.spb@gmail.com>
2020-03-03 19:30:48 +0300

    changed main page, added seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+USERS = ['Alex', 'Bob', 'John']
+LINK_TO_IMAGE = File.open('/Users/a1/Downloads/ruby/blog/app/assets/images/image1.jpg')
+
+
 Category.create(name: 'uncategorized', id: 1)
+
+#create random Users for db
+3.times do |t|
+  User.create(name: USERS[t - 1], email: "user#{t}@mail.ru", password: 'password', password_confirmation: 'password')
+end
+
+#create random Posts for db
+10.times do |i|
+  Post.create(title: "Пост номер #{i}", summary: "Краткое описание для поста #{i}", body: LOREM_IPSUM, user_id: rand(1..3), views: rand(6), category_id: 1, image: LINK_TO_IMAGE)
+end
